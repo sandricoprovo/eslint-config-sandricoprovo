47f3eb2d346c43dd76c25d385bb0766fa450035d
Sandrico Provo <Sprovo@outlook.com>
2020-11-07 15:58:12 -0400

    Fix: no-unused-vars changed to fix error

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
     'no-unused-vars': [
       1,
       {
-        ignoreSiblings: true,
+        ignoreRestSiblings: true,
         argsIgnorePattern: 'res|next|^err',
       },
     ],
