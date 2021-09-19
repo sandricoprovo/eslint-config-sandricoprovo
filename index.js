bc3db8d09bb6388c990e878278eb5aad71466f3a
Sandrico Provo <Sprovo@outlook.com>
2021-09-19 19:04:45 -0300

    refactor: Adds working typescript linting - CHANGE: Adds updated prettier rules - CHANGE: Turns off some base eslint rules when using prettier

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const eslintrc = require('./.eslintrc');
+const eslintrc = require('./.eslintrc.json');
 
 module.exports = eslintrc;
---
+++ typescript.js
@@ -0,0 +1,41 @@
+module.exports = {
+	globals: {
+		React: true,
+		JSX: true,
+	},
+	// These are all of the rule sets being extended
+	extends: [
+		'airbnb-typescript',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		// Extend rules from .eslintrc.js
+		'./.eslintrc.json',
+	],
+	// Allows ESlint to lint typescript code
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	// Typescript specific rules
+	rules: {
+		'@typescript-eslint/no-misused-promises': [
+			'error',
+			{
+				checksVoidReturn: false,
+			},
+		],
+		'@typescript-eslint/no-explicit-any': 'off',
+		'no-redeclare': 'off',
+		'@typescript-eslint/no-redeclare': [
+			'warn',
+			{
+				ignoreDeclarationMerge: true,
+			},
+		],
+		'@typescript-eslint/no-floating-promises': 'off',
+		'no-use-before-define': 'off',
+		'@typescript-eslint/no-use-before-define': ['error'],
+		'no-unused-vars': 'off',
+	},
+	parserOptions: {
+		project: './tsconfig.json',
+	},
+};
