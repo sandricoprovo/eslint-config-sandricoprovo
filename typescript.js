23767730732f95ee99d03bb42c116411f4440a99
Sandrico Provo <Sprovo@outlook.com>
2022-03-16 07:36:30 -0300

    [change]: Changes prettier config & turns off some react warnings not needed in typescript.

--- typescript.js
+++ typescript.js
@@ -1,56 +1,57 @@
 module.exports = {
-	globals: {
-		React: true,
-		JSX: true,
-	},
-	// These are all of the rule sets being extended
-	extends: [
-		'plugin:@typescript-eslint/recommended',
-		'airbnb-typescript',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:@typescript-eslint/recommended-requiring-type-checking',
-		// Extend rules from .eslintrc.js
-		'./.eslintrc.js',
-	],
-	// Allows ESlint to lint typescript code
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: './tsconfig.json',
-	},
-	plugins: ['@typescript-eslint'],
-	settings: {
-		'import/parsers': {
-			'@typescript-eslint/parser': ['.ts', '.tsx'],
-		},
-		'import/resolver': {
-			node: {
-				extensions: ['.ts', '.tsx', '.js', '.jsx'],
-			},
-			typescript: {
-				alwaysTryTypes: true,
-			},
-		},
-		'import/extensions': ['.ts', '.tsx', '.js', '.jsx'],
-	},
-	// Typescript specific rules
-	rules: {
-		'no-redeclare': 'off',
-		'import/no-unresolved': 'error',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'@typescript-eslint/no-use-before-define': 'off',
-		'@typescript-eslint/no-floating-promises': 'off',
-		'@typescript-eslint/no-var-requires': 0,
-		'@typescript-eslint/no-misused-promises': [
-			'error',
-			{
-				checksVoidReturn: false,
-			},
-		],
-		'@typescript-eslint/no-redeclare': [
-			'warn',
-			{
-				ignoreDeclarationMerge: true,
-			},
-		],
-	},
+    globals: {
+        React: true,
+        JSX: true,
+    },
+    // These are all of the rule sets being extended
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'airbnb-typescript',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        // Extend rules from .eslintrc.js
+        './.eslintrc.js',
+    ],
+    // Allows ESlint to lint typescript code
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.json',
+    },
+    plugins: ['@typescript-eslint'],
+    settings: {
+        'import/parsers': {
+            '@typescript-eslint/parser': ['.ts', '.tsx'],
+        },
+        'import/resolver': {
+            node: {
+                extensions: ['.ts', '.tsx', '.js', '.jsx'],
+            },
+            typescript: {
+                alwaysTryTypes: true,
+            },
+        },
+        'import/extensions': ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    // Typescript specific rules
+    rules: {
+        'react/prop-types': 'off',
+        'no-redeclare': 'off',
+        'import/no-unresolved': 'error',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/no-misused-promises': [
+            'error',
+            {
+                checksVoidReturn: false,
+            },
+        ],
+        '@typescript-eslint/no-redeclare': [
+            'warn',
+            {
+                ignoreDeclarationMerge: true,
+            },
+        ],
+    },
 };
