0f2f8b6e25618e4d82e3138076bfa8bff608d1c0
Sandrico Provo <Sprovo@outlook.com>
2022-12-28 21:27:26 -0400

    feat: updates eslint rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,12 @@ module.exports = {
     extends: ['airbnb', 'eslint:recommended'],
     parser: '@babel/eslint-parser',
     parserOptions: {
-        ecmaVersion: 2020,
+        ecmaVersion: 2022,
         requireConfigFile: false,
         ecmaFeatures: {
             jsx: true,
         },
+        sourceType: 'module',
         babelOptions: {
             presets: ['@babel/preset-react'],
         },
@@ -14,23 +15,35 @@ module.exports = {
     env: {
         browser: true,
         node: true,
-        jquery: true,
         jest: true,
     },
     plugins: ['html', 'react-hooks'],
     rules: {
+        import: 0,
+        radix: 0,
+        indent: 0,
         'no-use-before-define': 'off',
         'no-debugger': 0,
         'no-alert': 0,
         'no-await-in-loop': 0,
         'no-return-assign': ['error', 'except-parens'],
+        'no-redeclare': 'off',
+        'arrow-body-style': [2, 'as-needed'],
+        'import/order': ['warn', { 'newlines-between': 'always' }],
+        'no-console': 0,
+        'import/prefer-default-export': 0,
+        'func-names': 0,
+        'space-before-function-paren': 0,
+        'comma-dangle': 0,
+        'import/extensions': 0,
+        'no-underscore-dangle': 0,
+        'consistent-return': 0,
         'no-restricted-syntax': [
             2,
             'ForInStatement',
             'LabeledStatement',
             'WithStatement',
         ],
-        'no-redeclare': 'off',
         'no-unused-vars': [
             1,
             {
@@ -44,7 +57,6 @@ module.exports = {
                 destructuring: 'all',
             },
         ],
-        'arrow-body-style': [2, 'as-needed'],
         'no-unused-expressions': [
             2,
             {
@@ -57,43 +69,6 @@ module.exports = {
                 props: false,
             },
         ],
-        'max-len': [
-            'warn',
-            {
-                code: 100,
-                ignoreComments: true,
-                ignoreTrailingComments: true,
-                ignoreUrls: true,
-                ignoreStrings: true,
-                ignoreTemplateLiterals: true,
-                ignoreRegExpLiterals: true,
-            },
-        ],
-        'no-console': 0,
-        'import/prefer-default-export': 0,
-        import: 0,
-        'func-names': 0,
-        'space-before-function-paren': 0,
-        'comma-dangle': 0,
-        'import/extensions': 0,
-        'no-underscore-dangle': 0,
-        'consistent-return': 0,
-        'react/display-name': 1,
-        'react/no-array-index-key': 0,
-        'react/react-in-jsx-scope': 0,
-        'react/prefer-stateless-function': 0,
-        'react/forbid-prop-types': 0,
-        'react/no-unescaped-entities': 0,
-        'jsx-a11y/accessible-emoji': 0,
-        'react/require-default-props': 0,
-        'react/jsx-props-no-spreading': 0,
-        'react/jsx-filename-extension': [
-            2,
-            {
-                extensions: ['.js', '.jsx', '.ts', '.tsx'],
-            },
-        ],
-        radix: 0,
         'no-shadow': [
             2,
             {
@@ -101,24 +76,6 @@ module.exports = {
                 allow: ['resolve', 'reject', 'done', 'next', 'err', 'error'],
             },
         ],
-        quotes: [
-            2,
-            'single',
-            {
-                avoidEscape: true,
-                allowTemplateLiterals: true,
-            },
-        ],
-        'jsx-a11y/href-no-hash': 'off',
-        'jsx-a11y/anchor-is-valid': [
-            'warn',
-            {
-                aspects: ['invalidHref'],
-            },
-        ],
-        'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
-        'react/prop-types': 'warn',
         'import/no-extraneous-dependencies': [
             'error',
             {
@@ -132,12 +89,36 @@ module.exports = {
                 allowPattern: '^[a-z]+(_[a-z]+)+$',
             },
         ],
-        indent: 0,
+        // React
         'react/jsx-indent': 0,
         'react/jsx-indent-props': 0,
         'react/jsx-no-bind': 0,
         'react/jsx-one-expression-per-line': 0,
-        'import/order': ['warn', { 'newlines-between': 'always' }],
+        'jsx-a11y/href-no-hash': 'off',
+        'react-hooks/rules-of-hooks': 'error',
+        'react-hooks/exhaustive-deps': 'warn',
+        'react/prop-types': 'warn',
+        'react/display-name': 1,
+        'react/no-array-index-key': 0,
+        'react/react-in-jsx-scope': 0,
+        'react/prefer-stateless-function': 0,
+        'react/forbid-prop-types': 0,
+        'react/no-unescaped-entities': 0,
+        'jsx-a11y/accessible-emoji': 0,
+        'react/require-default-props': 0,
+        'react/jsx-props-no-spreading': 0,
+        'jsx-a11y/anchor-is-valid': [
+            'warn',
+            {
+                aspects: ['invalidHref'],
+            },
+        ],
+        'react/jsx-filename-extension': [
+            2,
+            {
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        ],
     },
     settings: {
         'import/extensions': ['.ts', '.tsx', '.js', '.jsx'],
